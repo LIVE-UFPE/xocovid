59a4d0bfeed3440a6135551ef48c9ceffaeaf066
Job <ljba@cin.ufpe.br>
2020-03-29 17:11:34 -0300

    FEITO, componentes podem ser feitos em vue e importados no template

---
+++ App/static/vue-views/main-app.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- <v-app> -->
+    <div>
+      <!-- // TODO responsividade! -->
+      <p>teste consciente</p>
+    </div>
+  <!-- </v-app> -->
+</template>
+
+<script>
+module.exports = {
+  name: 'main-app',
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Barlow, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.round-border {
+  border-radius: 5% !important;
+}
+</style>
--- covidWeb/settings.py
+++ covidWeb/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+DEBUG = True
+
+# TODO explicar isso aqui
+if DEBUG:
+    import mimetypes
+    mimetypes.add_type("application/javascript", ".js", True)
+    # mimetypes.add_type("application/vue", ".vue", True)
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
