b8cc2cc94368c83395f35b553d5f5aee47a486c0
Job <ljba@cin.ufpe.br>
2020-03-30 00:18:07 -0300

    resta consertar a hierarquia do CSS

---
+++ App/static/vue-views/login.vue
@@ -0,0 +1,97 @@
+<template>
+    
+    <!-- // TODO resolve validation-->
+    <v-container fluid class="d-flex pa-0 ma-0" fill-height>
+        <v-text-field 
+            required 
+            placeholder="Email"
+            v-model="email"
+            outlined 
+            full-width
+            height="100px" 
+            single-line solo
+        ></v-text-field> 
+        <!-- // TODO detail more -->
+
+        <v-text-field
+            required 
+            placeholder="Senha"
+            v-model="password"
+            outlined 
+            full-width
+            type="password"
+            height="100px" 
+            single-line solo
+        ></v-text-field> 
+
+
+        <v-btn
+            color="primary"
+            class="mt-8 mb-0"
+            @click="submit"
+            height="100px"
+            width="100%"
+        >
+
+            <input type="submit" name="" value="Entrar">
+            <!-- // TODO fix alignment -->
+        </v-btn>
+        
+        <v-btn text class="mt-7 sub-botao d-block" width="100%" >Cadastrar</v-btn>
+        <v-btn text class="mt-6 sub-botao d-block" width="100%" >Esqueci minha senha</v-btn>
+    </v-container>
+                
+</template>
+
+<script>
+// ! componentes devem ser exportados dessa forma, para que o httpVueLoader funcione
+
+module.exports = {
+  name: 'login',
+  methods: {
+      submit () {
+          console.log(`email é ${this.email} e senha ${this.password}`)
+      },
+  },
+  data: () => ({
+      email: null,
+      password: null,
+  }),
+}
+</script>
+
+<style scoped>
+.v-text-field, .v-btn {
+    font-size: 30px;
+    border-radius: 10px;
+}
+
+.sub-botao{
+    font-size: 25px;
+    font-weight: lighter;
+    text-transform: none;
+    letter-spacing: normal;
+}
+
+#top {
+  font-family: Barlow, Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.round-border {
+  border-radius: 23px !important;
+}
+</style>
--- App/views.py
+++ App/views.py
@@ -14,26 +14,6 @@ def index(request):
 def tela_exemplo(request, id):
     return render(request, 'exemplo/tela_exemplo.html', {'id':id,})
 
-    
-"""
-! Funcionamento das views
-
-Views nada mais é que um módulo python que agrupa um conjunto de ações.
-views em django são divididas em dois tipos: views baseadas em FUNCTION e views baseadas em CLASS
-
-* Function based View
-views baseadas em funções sao feitas usando uma função em python:
-    1. função recebe como argumento um objeto HttpRequest
-    2. função retorna um objeto HttpResponse
-
-são divididas em 4 estratégias básicas (CRUD):
-? Create // Retrieve // Update // Delete
-CRUD é a base de qualquer framework
-
-! Só que tem mais um detalhe!!!
-para acessar essa função, devemos especificar uma rota através do sistema de rotas do Django.
-"""
-
 @login_required
 def user_logout(request):
     logout(request)
@@ -57,6 +37,7 @@ def register(request):
 
 def user_login(request):
     if request.method == 'POST':
+        print('tested2')
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(username=username, password=password)
@@ -72,3 +53,22 @@ def user_login(request):
             return HttpResponse("Invalid login details given")
     else:
         return render(request, 'login.html', {})
+
+"""
+! Funcionamento das views
+
+Views nada mais é que um módulo python que agrupa um conjunto de ações.
+views em django são divididas em dois tipos: views baseadas em FUNCTION e views baseadas em CLASS
+
+* Function based View
+views baseadas em funções sao feitas usando uma função em python:
+    1. função recebe como argumento um objeto HttpRequest
+    2. função retorna um objeto HttpResponse
+
+são divididas em 4 estratégias básicas (CRUD):
+? Create // Retrieve // Update // Delete
+CRUD é a base de qualquer framework
+
+! Só que tem mais um detalhe!!!
+para acessar essa função, devemos especificar uma rota através do sistema de rotas do Django.
+"""
