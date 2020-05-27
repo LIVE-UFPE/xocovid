1a90e55e1b344a26ece4248995f66b1f0f0080e2
Lucas Job <you@example.com>
2020-05-26 22:55:58 -0300

    ajustes implementados

--- App/static/map/StateMap.vue
+++ App/static/map/StateMap.vue
@@ -1,5 +1,9 @@
 <template>
     <div id="map">
+        <v-snackbar v-model="snackbar" top >
+            {{ txtsnack }}
+            <v-btn text color="white" @click="snackbar = false" >Ok</v-btn>
+        </v-snackbar>
     </div>
 </template>
 <script>
@@ -15,6 +19,9 @@ module.exports ={
             map: null,
             geojson: null,
             style: null,
+            ultimoscasos: [],
+            txtsnack: 'Oi',
+            snackbar: false,
         } 
     },
     // ? como props aq é um objeto, não é possível dar watch diretamente nas propriedades de prop, para isso, usamos uma computed property e damos watch nela. vale citar também que as props são acessadas por "this.pins", por exemplo, diretamente em qualquer porção de código no script
@@ -44,8 +51,16 @@ module.exports ={
                         this.maiscasos = this.casos[0]['quantidade_casos']
                         this.menoscasos = this.casos[this.casos.length - 1]['quantidade_casos']
                         console.log(`maiscasos = ${this.maiscasos} e menoscasos = ${this.menoscasos}`)
+                        this.casos.forEach(element => {
+                            this.ultimoscasos[element['estado_residencia']] = element['quantidade_casos']
+                        });
+                        // console.log(this.ultimoscasos)
                         this.geojson.setStyle(this.style)    
-                    }else console.log('não tem dado presse dia lul')
+                    }else {
+                        this.txtsnack = 'Não há casos pra esse dia, mantendo os números do último dia com dados'
+                        this.snackbar = true
+                        // console.log(this.ultimoscasos)
+                    }
                     
                 }
             })
@@ -93,12 +108,13 @@ module.exports ={
 
             //DEBUG se der certo, tirar mais e menoscasos e deixar so a media
             let media = (that.maiscasos + that.menoscasos) / 2
-            console.log(media)
+            // console.log(media)
             try {
                 d = d['quantidade_casos']
             } catch (error) {
-                console.log(`não temos informações sobre ${estado} nesse dia,inserindo cor fora do padrão`)
-                return '#0011ff'
+                this.txtsnack = `não temos informações sobre ${estado} nesse dia,mantendo ultimos dados obtidos`
+                this.snackbar = true
+                d = that.ultimoscasos[estado]
             }
             return d >= Math.floor(media * 0.875) ? '#ff0000' : // * tons de vermelho
                 d >= Math.floor(media * 0.75)  ? '#ff4242' :
@@ -107,7 +123,8 @@ module.exports ={
                 d >= Math.floor(media * 0.375)   ? '#ffabab' :
                 d >= Math.floor(media * 0.25)   ? '#ffbaba' :
                 d >= Math.floor(media * 0.125)   ? '#ffd000' : // * tons de amarelo
-                          '#ffe054' // * AMARELO MEMSO TUDO AMARELO NISSO AQ
+                d >= Math.floor(media * 0.03125)   ? '#ffe054' : // * AMARELO MEMSO TUDO AMARELO NISSO AQ
+                    '#38ff26'
         }
         function style(feature) {
             return {
@@ -135,7 +152,7 @@ module.exports ={
                     casos = that.casos.find( elem => elem['estado_residencia'] === props.name)['quantidade_casos']
                 } catch (error) {
                     console.log('sem dados')
-                    casos = 'S/ Info'
+                    casos = that.ultimoscasos[props.name]
                 }    
             }
             this._div.innerHTML = '<h4>Número de casos acumulados</h4>' +  (props ?
